e9c35e513c5cbe6f084d3f88a11a331513ab29e8
jquerycreator <jquerycreator@mail.com>
2019-07-11 11:57:44 +0400

    first commit

--- ДЗ_3/script.js
+++ ДЗ_3/script.js
@@ -1,32 +1,54 @@
-console.log('-----------------1 Задание-------------------')
-var AgeActer=0,CountActer=0
-for (film of films){
-	if(film.director.oscarsCount==0){
-		CountActer+=film.actors.length
-		for(acter of film.actors){
-			AgeActer+=acter.age
-		}
-	}
-}
-console.log('Средний возраст актеров которые снимались в фильмах и не получили оскара:',parseInt(AgeActer/CountActer))
-console.log('---------------------------------------------')
-console.log('---------------2 Задание--------------------')
-for(film of films){
-  if (film.creationYear>1995 && film.actors.some((acter)=>{
-      return acter.name=="Tom Hanks"})){
-      console.log("Фильм:",film.title)
-      console.log("Актеры:");film.actors.forEach((item,index)=>{if(item.name!="Tom Hanks")console.log( index+")"+item.name+" ")})
-  }
-}
-console.log('--------------------------------------------')
+const App = {
+  firstTask:function(){
+     console.log('-----------------1 Задание-------------------')
+     var AgeActer=CountActer=0;
+     for (film of films){
+       if(film.director.oscarsCount==0){
+         CountActer+=film.actors.length
+         for(acter of film.actors){
+           AgeActer+=acter.age
+        }
+	  }
+    }
+    console.log('AvgAge:',parseInt(AgeActer/CountActer))
+    console.log('---------------------------------------------')
+    },
+  TwoTask:function(){
+   	       console.log('---------------2 Задание--------------------')
+   	       for(film of films)
+              if (film.creationYear>1995 && film.actors.some((acter)=>{
+              return acter.name=="Tom Hanks"})){
+                console.log("Фильм:",film.title)
+                console.log("Актеры:");
+                film.actors.forEach((item,index)=>{
+             	               if(item.name!="Tom Hanks")console.log( index+")"+item.name+" ")
+             	            })
+                  }
+            console.log('--------------------------------------------')
+  },
+  ThreeTask:function(){
+      console.log('---------------3 Задание--------------------')
+      let budgetSum=0;
+      for(film of films){
+         if (film.director.age<70 && !film.actors.some((acter)=>{
+         return acter.name=="Tom Hanks"})){
+          budgetSum+=Number((film.budget.slice(1,film.budget.length)).split(' ').join(''))
+        }
+      }
+      console.log('Сумарный бюджет фильмов:$',budgetSum)
+   },
+  start:function(){
+     this.firstTask();
+     this.TwoTask();
+     this.ThreeTask();
+   }
+ }
+App.start()
 
-console.log('--------------------3 Задание-----------------')
-var budgetSum=0;
-for(film of films){
-  if (film.director.age<70 && !film.actors.some((acter)=>{
-      return acter.name=="Tom Hanks"})){
-        budgetSum+=Number((film.budget.slice(1,film.budget.length)).split(' ').join(''))
-  }
-}
-console.log('Сумарный бюджет фильмов:$',budgetSum)
-console.log('---------------------------------------------')
+
+
+
+
+
+
+
